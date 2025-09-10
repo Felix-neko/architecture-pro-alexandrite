9fc2939cb2441e79e997e42675e2a5a0e664fdae
Алексей Головизин <agolovizin@alfabank.ru>
2025-09-10 23:17:09 +0300

    Пытаемся завести ELK на кубере...

---
+++ misc/kube_elk_experiments/install_elk.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+
+kubectl create namespace web-services
+kubectl apply -f $BASEDIR/nginx-stateful-nodeport.yaml -n web-services
+
+kubectl create namespace logging
+
+helm repo add elastic https://helm.elastic.co
+helm repo update
+
+helm install elasticsearch elastic/elasticsearch -n logging \
+  --set replicas=1 \
+  --set resources.requests.memory=2Gi
+
+helm install kibana elastic/kibana -n logging \
+  --set service.type=ClusterIP
+
+helm install filebeat elastic/filebeat -n logging -f $BASEDIR/filebeat-values.yaml
---
+++ misc/kube_elk_experiments/uninstall_elk.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+
+delete namespace logging
+delete namespace web-services
