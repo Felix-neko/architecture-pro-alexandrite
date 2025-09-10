551f8064e82af6a6508bf5eec698021fdbedaa99
Алексей Головизин <agolovizin@alfabank.ru>
2025-09-11 00:40:37 +0300

    Промежуточный коммит...

--- misc/kube_elk_experiments/install_elk.sh
+++ misc/kube_elk_experiments/install_elk.sh
@@ -9,12 +9,10 @@ kubectl create namespace logging
 helm repo add elastic https://helm.elastic.co
 helm repo update
 
-helm install elasticsearch elastic/elasticsearch -n logging \
-  --set replicas=1 \
-  --set resources.requests.memory=2Gi \
-  --set esConfig."elasticsearch\.yml"="xpack.security.enabled: false"
+helm install elasticsearch elastic/elasticsearch -n logging -f $BASEDIR/elasticsearch-values.yaml
 
-helm install kibana elastic/kibana -n logging \
-  --set service.type=ClusterIP
+helm install kibana elastic/kibana -n logging -f $BASEDIR/kibana-values.yaml
 
 helm install filebeat elastic/filebeat -n logging -f $BASEDIR/filebeat-values.yaml
+
+kubectl apply -f $BASEDIR/kibana-nodeport.yaml -n logging
